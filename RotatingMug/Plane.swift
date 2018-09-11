184c1088f81958041cf190bea81b38e333b771fd
Vikash Kumar <vkyvikash@Vikashs-MBP.Dlink>
2018-09-11 21:23:55 +0530

    Placing mug

--- RotatingMug/Plane.swift
+++ RotatingMug/Plane.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+import ARKit
+
+class Plane: SCNNode {
+    var plane: SCNPlane
+    
+    init(anchor: ARPlaneAnchor) {
+        plane = SCNPlane(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z))
+        super.init()
+        
+        plane.cornerRadius = 0.005
+        
+        // ab build and add an SCNode using this plane
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.position = SCNVector3Make(anchor.center.x, 0, anchor.center.z)
+        planeNode.eulerAngles.x = -Float.pi/2
+        planeNode.opacity = 0.15
+        addChildNode(planeNode)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // Code which is called whenever the anchor updates
+    func updateWith(anchor: ARPlaneAnchor) {
+        plane.width = CGFloat(anchor.extent.x)
+        plane.height = CGFloat(anchor.extent.z)
+        
+        position = SCNVector3Make(anchor.center.x, 0, anchor.center.z)
+    }
+    
+    func hidePlane() {
+        // TODO implement this
+//        planeNode.geometry?.materials.first?.diffuse.contents = UIColor(white: 1, alpha: 0)
+    }
+}
--- RotatingMug/ViewController.swift
+++ RotatingMug/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     var currentAngleY: Float = 0
     
+    private var planes = [UUID: Plane]() // Dictionary of planes
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,21 +26,61 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints] // This is how you show feature points
+        sceneView.antialiasingMode = .multisampling4X
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/CoffeeMug/model.dae")!
-        self.sceneView?.autoenablesDefaultLighting = true
-        self.sceneView.scene = scene
+//        let scene = SCNScene(named: "art.scnassets/CoffeeMug/model.dae")!
+//        self.sceneView?.autoenablesDefaultLighting = true
+//        self.sceneView.scene = scene
+//
+        //      TODO: Should be done like below line, instead of directly calling childNodes[0]
+        // let shipNode = scene.rootNode.childNode(withName: "model", recursively: true)!
+//        let mugNode = scene.rootNode.childNodes[0]
+//        mugNode.scale = SCNVector3(0.01, 0.01, 0.01)
+//
+//        let (minVec, maxVec) = mugNode.boundingBox
+//        mugNode.pivot = SCNMatrix4MakeTranslation((maxVec.x - minVec.x) / 2 + minVec.x, 0, (maxVec.z - minVec.z) / 2 + minVec.z)
         
-//        let shipNode = scene.rootNode.childNode(withName: "model", recursively: true)!
-        let mugNode = scene.rootNode.childNodes[0]
-        mugNode.scale = SCNVector3(0.01, 0.01, 0.01)
-        
-        let (minVec, maxVec) = mugNode.boundingBox
-        mugNode.pivot = SCNMatrix4MakeTranslation((maxVec.x - minVec.x) / 2 + minVec.x, 0, (maxVec.z - minVec.z) / 2 + minVec.z)
-        
+        // TODO Fix rotate functionality
         let rotateGesture = UIPanGestureRecognizer(target: self, action: #selector(rotateModels(_:)))
         sceneView.addGestureRecognizer(rotateGesture)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTapScene(_:)))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func didTapScene(_ sender: UITapGestureRecognizer) {
+        guard sender.state == .ended else {
+            return
+        }
+        
+        let point = sender.location(in: self.sceneView)
+        let results = self.sceneView.hitTest(point, types: [.existingPlaneUsingExtent, .estimatedHorizontalPlane])
+        
+        
+        if let match = results.first {
+            let scene = SCNScene(named: "art.scnassets/CoffeeMug/model.dae")!
+        
+            //      TODO: Should be done like below line, instead of directly calling childNodes[0]
+            // let shipNode = scene.rootNode.childNode(withName: "model", recursively: true)!
+            let mugNode = scene.rootNode.childNodes[0]
+            let t = match.worldTransform
+            mugNode.position = SCNVector3(x: t.columns.3.x, y: t.columns.3.y, z: t.columns.3.z)
+            mugNode.scale = SCNVector3(0.02, 0.02, 0.02)
+            
+            let (minVec, maxVec) = mugNode.boundingBox
+            mugNode.pivot = SCNMatrix4MakeTranslation((maxVec.x - minVec.x) / 2 + minVec.x, 0, (maxVec.z - minVec.z) / 2 + minVec.z)
+            
+            // TODO remove this hack. You should be able to get the exact planeNode and remove that
+            self.sceneView.scene.rootNode.enumerateChildNodes { (existingNode, _) in
+                guard (existingNode.geometry as? SCNPlane) != nil else {
+                    return
+                }
+                existingNode.removeFromParentNode()
+            }
+            self.sceneView.scene.rootNode.addChildNode(mugNode)
+        }
     }
     
     @objc func rotateModels(_ gesture: UIPanGestureRecognizer) {
@@ -67,6 +109,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
         configuration.isLightEstimationEnabled = true
 
         // Run the view's session
@@ -95,6 +138,36 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         return node
     }
 */
+    
+    // 3 renderers overwrite karne hn..
+    // 1. when ARPlaneAnchor is added
+    // 2. when ARPlaneAnchor is updated
+    // 3. when Anchor is removed
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {
+            return
+        }
+        
+        let plane = Plane(anchor: planeAnchor)
+        planes[planeAnchor.identifier] = plane
+        node.addChildNode(plane)
+        
+        print("Found plane \(planeAnchor)")
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {
+            return
+        }
+        if let plane = planes[planeAnchor.identifier] {
+            plane.updateWith(anchor: planeAnchor)
+        }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        planes.removeValue(forKey: anchor.identifier)
+    }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
